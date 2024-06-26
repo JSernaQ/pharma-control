5e36ac01a7f883f4a6be1bfadee542fbf39bb0b6
Juan Serna <jsernaq02@gmail.com>
2024-06-26 12:33:33 -0500

    Sidebar finished

--- src/controllers/sale.controller.js
+++ src/controllers/sale.controller.js
@@ -2,10 +2,12 @@ const { Product } = require('../models/product.model');
 const { Customer } = require('../models/customer.model');
 
 const salePageGet = async (req, res) => {
+    const msg = req.query.msg || undefined
+    const error = req.query.error || undefined
     try {
         const products = await Product.find();
         const customers = await Customer.find();
-        res.render('sales', { customers, products });
+        res.render('sales', { customers, products, msg, error });
     } catch (error) {
         console.error('Error al obtener productos y clientes:', error);
         res.status(500).send('Error interno del servidor');
